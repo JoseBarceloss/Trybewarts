4ec4b512a8a7a743aa55aada33bf67df9f70e646
Barcelos <barcelosps41710@gmail.com>
2023-04-26 17:41:25 -0300

    exercicio 3 completo

---
+++ script.js
@@ -0,0 +1,13 @@
+const formulario = document.querySelector('.trybewarts-login');
+const email = document.querySelector('.Email');
+const password = document.querySelector('.senha');
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (email.value === 'tryber@teste.com' && password.value === '123456') {
+        alert("Olá, Tryber!");
+    } else {
+        alert("Email ou senha inválidos.");
+    }
+});
